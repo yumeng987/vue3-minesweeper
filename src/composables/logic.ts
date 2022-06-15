413d480b0eff7f292f6f2609c2ff68cf96db3c30
余檬 <1581745002@qq.com>
2022-06-15 16:53:27 +0800

    feat:lost alert

--- src/composables/logic.ts
+++ src/composables/logic.ts
@@ -249,7 +249,11 @@ export class GamePlay {
   onGameOver(status: GameStatus) {
     this.state.value.status = status
     this.state.value.endMS = +Date.now()
-    if (status === 'lost')
+    if (status === 'lost') {
       this.showAllMines()
+      setTimeout(() => {
+        alert('lost')
+      }, 30)
+    }
   }
 }
